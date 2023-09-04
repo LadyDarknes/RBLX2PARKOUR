1b085b4c88c2d8f54f6e104c1a4720f55480dd54
Lady of Darkness <85314429+LadyDarknes@users.noreply.github.com>
2023-09-04 22:10:26 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,29 @@
+local function Import_Files()
+    if isfolder("RBLX2PARKOUR") then
+        delfolder("RBLX2PARKOUR")
+        Import_Files()
+        return
+    end
+
+    makefolder("RBLX2PARKOUR")
+    makefolder("RBLX2PARKOUR/Modules")
+    makefolder("RBLX2PARKOUR/Source")
+
+    writefile("RBLX2PARKOUR/Modules/Util.lua", game:HttpGetAsync("https://raw.githubusercontent.com/xgamerman626/Hub/main/Modules/Util.lua"))
+    writefile("RBLX2PARKOUR/Modules/UI.lua", game:HttpGetAsync("https://raw.githubusercontent.com/xgamerman626/Hub/main/Modules/UI.lua"))
+    writefile("RBLX2PARKOUR/Modules/ACB.lua", game:HttpGetAsync("https://raw.githubusercontent.com/xgamerman626/Hub/main/Modules/ACB.lua"))
+    writefile("RBLX2PARKOUR/Source/Runtime.lua", game:HttpGetAsync("https://raw.githubusercontent.com/xgamerman626/Hub/main/Source/Runtime.lua"))
+end
+
+local function Init()
+    Import_Files()
+
+    repeat
+        task.wait()
+    until isfile("RBLX2PARKOUR/Source/Runtime.lua")
+
+    local Runtime = loadstring(readfile("RBLX2PARKOUR/Source/Runtime.lua"))()
+    Runtime.Init()
+end
+
+Init()
