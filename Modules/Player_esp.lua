7c5cde505c75598f49e51167434c989091b48ae0
Lady of Darkness <85314429+LadyDarknes@users.noreply.github.com>
2023-09-05 22:33:28 +0300

    Update Player_esp.lua

--- Modules/Player_esp.lua
+++ Modules/Player_esp.lua
@@ -1,40 +1,42 @@
-local color = BrickColor.new(50,0,250)
-local transparency = .8
+local RarityColours = {
+    ["Common"] = Color3.fromRGB(73, 30, 30), -- Brown
+    ["Uncommon"] = Color3.fromRGB(0, 214, 0), -- Green
+    ["Rare"] = Color3.fromRGB(51, 18, 112), -- Purple
+    ["Epic"] = Color3.fromRGB(5, 154, 168), -- Blue
+    ["Legendary"] = Color3.fromRGB(254, 254, 0), -- Yellow
+    ["Ultimate"] = Color3.fromRGB(255, 0, 0), -- Red
+    ["Resplendent"] = Color3.fromRGB(255, 255, 255), -- White
+};
 
-local Players = game:GetService("Players")
-local function _ESP(c)
-  repeat wait() until c.PrimaryPart ~= nil
-  for i,p in pairs(c:GetChildren()) do
-    if p.ClassName == "Part" or p.ClassName == "MeshPart" then
-      if p:FindFirstChild("shit") then p.shit:Destroy() end
-      local a = Instance.new("BoxHandleAdornment",p)
-      a.Name = "shit"
-      a.Size = p.Size
-      a.Color = color
-      a.Transparency = transparency
-      a.AlwaysOnTop = true    
-      a.Visible = true    
-      a.Adornee = p
-      a.ZIndex = true    
+local function AddBillboard(Part)
+    local Colour = RarityColours[Part.Parent.Rarity.Value] or Color3.fromRGB(84, 84, 84);
 
-    end
-  end
+    local BillboardGui = Instance.new("BillboardGui", Part);
+    BillboardGui.Size = UDim2.new(15, 0, 15, 0);
+    BillboardGui.LightInfluence = 1;
+    BillboardGui.AlwaysOnTop = true;
+
+    local Frame = Instance.new("Frame", BillboardGui);
+    Frame.Size = UDim2.new(1, 0, 1, 0);
+    Frame.BorderSizePixel = 0;
+    Frame.BackgroundColor3 = Colour;
 end
-local function ESP()
-  for i,v in pairs(Players:GetChildren()) do
-    if v ~= game.Players.LocalPlayer then
-      if v.Character then
-        _ESP(v.Character)
-      end
-      v.CharacterAdded:Connect(function(chr)
-        _ESP(chr)
-      end)
+
+while true do
+    for _, Bag in pairs(game.Workspace:GetChildren()) do
+        -- Making sure we're only looking at the bags
+        if Bag.ClassName ~= "Model" then continue; end
+        if not Bag:FindFirstChild("BagTouchScript") then continue; end
+
+        -- Deleting the old BillboardGui if it exists
+        if Bag.Main:FindFirstChild("BillboardGui") then 
+            Bag.Main:FindFirstChild("BillboardGui"):Destroy(); 
+        end
+
+        if getgenv().enabled then
+            AddBillboard(Bag.Main);
+        end
     end
-  end
-  Players.PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Connect(function(chr)
-      _ESP(chr)
-    end)  
-  end)
+
+    wait(2);
 end
-ESP()
